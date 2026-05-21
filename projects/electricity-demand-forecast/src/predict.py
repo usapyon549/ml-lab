777399f87b7d2c961c13c90bf1150f3fc6113fe9
Damon <101724819+usapyon549@users.noreply.github.com>
2026-05-21 19:29:09 +0900

    feat: 予測に用いる入力データをコマンド引数で受けつけるように変更

--- projects/electricity-demand-forecast/src/predict.py
+++ projects/electricity-demand-forecast/src/predict.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 import os
 import pickle
+import sys
 
 import pandas as pd
 import numpy as np
@@ -43,15 +45,27 @@ def predict(model: lgb.LGBMRegressor, df: pd.DataFrame) -> None:
 def main():
 
     # docker動作時にパスの確認用。
-    print("カレントディレクトリ", os.getcwd())
+    # print("カレントディレクトリ", os.getcwd())
 
+    # 引数チェック
+    if len(sys.argv) < 2:
+        raise ValueError("input path required")
+
+    # 予測に必要なデータを読み込む際に、ローカルとdockerで共通の引数を扱う。パス問題解決のためにルートを取得
+    ROOT = Path(__file__).resolve().parent.parent
+    # print("root: ", ROOT)
+    path_input = os.path.join(ROOT, sys.argv[1])
+
+    # 現状、一個のモデルを読む。
+    path_model = os.path.join(ROOT, "models", "model.pkl")
+
+    # 引数でパス指定。
+    df = load_data(path=path_input)
     # df = load_data(path="../data/processed/dataset/X_test.csv")
-    # docker 用。  
-    df = load_data(path="data/processed/dataset/X_test.csv")
   
+    # 引数でパス指定。
+    model = load_model(path=path_model)
     # model = load_model(path="../models/model.pkl")
-    # docker用。
-    model = load_model(path="models/model.pkl")
 
     predict(model=model, df=df)
 
